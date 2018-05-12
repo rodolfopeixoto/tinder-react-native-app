d5a880c97d0ce524d9307a64b5cea2fa3977828e
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2018-05-12 10:49:59 -0300

    Create folder and files structure

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './styles.js';
+import Home from './screens/Home';
+import { Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Home />
+      </View>
+    );
+  }
+}
