cf3ff14f60126f0f6c69b883de1c94f3d423744a
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2018-05-13 10:11:41 -0300

    Integration Facebook login API

--- screens/Home.js
+++ screens/Home.js
@@ -3,6 +3,7 @@ import styles from '../styles';
 import {
  Text,
  View,
+ Alert
 } from 'react-native';
 import { connect } from 'react-redux';
 import { login } from '../redux/actions'; 
@@ -14,7 +15,23 @@ class Home extends Component{
   
   componentWillMount() {
     this.props.dispatch(login())
+    this.login()
   }
+
+  login = async () => {
+    const { type, token } = await Expo.Facebook.logInWithReadPermissionsAsync('1812020642439167', {
+      permissions: ['public_profile'],
+    });
+    if(type === 'success'){
+      const response = await fetch(
+      `https://graph.facebook.com/me?access_token=${token}`);
+      Alert.alert(
+        'Logged in!',
+        `Hi ${(await response.json()).name}!`, 
+     );
+    }
+  }
+
 
   render(){
     return(
